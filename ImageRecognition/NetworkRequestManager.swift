9af16a63677d5ef4abf35fdf727ecd853efd3460
OmerrKose <omerr.kose99@gmail.com>
2024-01-23 01:50:47 +0300

    Added search in web

---
+++ ImageRecognition/NetworkRequestManager.swift
@@ -0,0 +1,13 @@
+//
+//  SearchWebHelper.swift
+//  ImageRecognition
+//
+//  Created by Ömer Köse on 23.01.2024.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkRequestManager {
+    
+}
---
+++ ImageRecognition/SearchWebViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SearchWebViewController.swift
+//  ImageRecognition
+//
+//  Created by Ömer Köse on 23.01.2024.
+//
+
+import Foundation
